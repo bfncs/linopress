fe2f7a047c0096f74f4c813b1e6c76ae58443aed
Marc Löhe <marc@marcloehe.de>
2017-02-21 08:28:26 +0100

    Add wrapper for static rendering

---
+++ src/components/HtmlWrapper.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react';
+
+const HtmlWrapper = ({ children, state, head, scripts, stylesheets }) => {
+  // eslint-disable-next-line react/no-danger
+  const content = (<div id="root" dangerouslySetInnerHTML={{ __html: children }} />);
+  const htmlAttributes = head.htmlAttributes.toComponent();
+
+  return (
+    <html lang="en" {...htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {head.title.toComponent()}
+      {head.meta.toComponent()}
+      {head.link.toComponent()}
+      {
+        stylesheets.map(path => (
+          <link href={`/${path}`} rel="stylesheet" />
+        ))
+      }
+    </head>
+    <body>
+    { content }
+    <script
+      dangerouslySetInnerHTML={{__html: `window.__PRELOADED_STATE__ = ${JSON.stringify(state).replace(/</g, '\\u003c')};`}}
+    />
+    </script>
+    {
+      scripts.map(path => (
+        <script src={`/${path}`} type="text/javascript"></script>
+      ))
+    }
+    </body>
+    </html>
+  );
+};
+
+HtmlWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+  state: PropTypes.object.isRequired,
+  head: PropTypes.object.isRequired,
+  scripts: PropTypes.arrayOf(PropTypes.string),
+  stylesheets: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default HtmlWrapper;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
+import Helmet from 'react-helmet';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './redux/rootReducer';
-
+import HtmlWrapper from './components/HtmlWrapper';
 import Frontend from './FrontendApp';
 import './index.css';
 
@@ -28,7 +29,10 @@ if (typeof document !== 'undefined') {
       renderApp(createApp(Editor));
     });
   } else {
-    renderApp(createApp(Frontend));
+    const preloadedState = window.__PRELOADED_STATE__;
+    delete window.__PRELOADED_STATE__;
+
+    renderApp(createApp(Frontend, preloadedState));
   }
 }
 
@@ -36,8 +40,27 @@ export default (locals, callback) => {
   window.location = {
     pathname: locals.path,
   };
+
   const preloadedState = {
     page: locals.content[locals.path],
   };
-  callback(null, ReactDOMServer.renderToString(createApp(Frontend, preloadedState)));
+
+  const head = Helmet.rewind();
+  const content = ReactDOMServer.renderToString(createApp(Frontend, preloadedState));
+  const assets = Object.keys(locals.webpackStats.compilation.assets);
+  const stylesheets = assets.filter(value => value.match(/\.css$/));
+  const scripts = assets.filter(value => value.match(/\.js$/));
+
+  const page = ReactDOMServer.renderToStaticMarkup(
+    <HtmlWrapper
+      head={head}
+      stylesheets={stylesheets}
+      scripts={scripts}
+      state={preloadedState}
+    >
+      {content}
+    </HtmlWrapper>
+  );
+
+  callback(null, page);
 };
